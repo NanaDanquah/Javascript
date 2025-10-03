19b13e0c2d40b6e15e2f0fcce4de237b3377450e
Gabriel <danquahbadugabriel@gmail.com>
2025-10-03 20:46:03 +0000

    Loops: for loop and while loop

---
+++ Loops/index.js
@@ -0,0 +1,38 @@
+
+// below is an example of a for loop that counts from 1 to 5
+for (var i=1; i<6; i++) {
+    console.log(i);
+};
+console.log("Counting completed");
+
+//for loop that counts from 5 to 1
+for (var i=5; i>0; i--){
+    console.log(i);
+};
+console.log("Counting completed");
+
+
+//while loop that counts from 1 to 5
+var counter = 1;
+while (counter < 6){
+    console.log(counter);
+    counter += 1;
+};
+console.log("Counting completed");
+
+
+//while loop that counts down from 5 to 1
+var counter = 5;
+while (counter > 0) {
+    console.log(counter);
+    counter -= 1
+};
+console.log("Countdown Completed");
+
+
+//while loop that peints all the years from 2018 to 2022
+var year = 2018;
+while (year < 2023) {
+    console.log(year);
+    year += 1;
+};
