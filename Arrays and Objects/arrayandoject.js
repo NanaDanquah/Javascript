6af5fbbc201c8ecf46ed65ea54ed6df8c98cea7d
Gabriel <danquahbadugabriel@gmail.com>
2025-10-04 18:48:56 +0000

    arrays and objects

---
+++ Arrays and Objects/arrayandoject.js
@@ -0,0 +1,13 @@
+var clothes = [];
+clothes.push("LV")
+clothes.push("Gucci")
+clothes.push("Lauren")
+clothes.push("Polio")
+clothes.push("Sark")
+
+clothes.pop();
+console.log(clothes[2]);
+
+var favCar = {};
+favCar.convertible = true;
+console.log(favCar)
